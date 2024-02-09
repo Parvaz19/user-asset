92b52db2bf1e2d322f7e3b948d68d6e753e10049
mohsen <ahmadi>
2024-02-09 22:29:58 +0330

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AssetController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\BalanceController;
 use App\Http\Controllers\Api\V1\ConversionFactorController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\SettingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('guest')->prefix('v1')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
 });
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('assets', AssetController::class);
     Route::apiResource('conversion-factors', ConversionFactorController::class);
+    Route::apiResource('settings', SettingController::class);
+
+    Route::get('balances', [BalanceController::class, 'index'])->name('balances.index');
+    Route::post('balances/convert', [BalanceController::class, 'convert'])->name('balances.convert');
+    Route::get('balances/{asset}', [BalanceController::class, 'show'])->name('balances.show');
+    Route::patch('balances/{asset}', [BalanceController::class, 'update'])->name('balances.update');
+    Route::get('balances/log/{asset}', [BalanceController::class, 'log'])->name('balances.log');
+
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
